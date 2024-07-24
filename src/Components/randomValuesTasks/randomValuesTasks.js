82ffe47c1f33a8ab555dff0a376dbb813ed91498
marinaPerezYnov <148948211+marinaPerezYnov@users.noreply.github.com>
2024-07-24 15:17:08 +0200

    Updates

--- src/Components/randomValuesTasks/randomValuesTasks.js
+++
@@ -1,5 +0,0 @@
-// Composant dédié à la génération de valeurs associées aux tâches à faire dans la semaine
-
-import React, { useState, useEffect } from "react";
-
-export default function RandomValuesTasks() {}
--- src/Components/trello/Constants.js
+++
@@ -1,6 +0,0 @@
-export const COLUMN_NAMES = {
-    DO_IT: 'A faire',
-    IN_PROGRESS: 'En cours',
-    AWAITING_REVIEW: 'Revue en attente',
-    DONE: 'Terminée',
-}
