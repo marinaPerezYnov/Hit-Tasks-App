65ef9993724169cb310e01b6eac93b782a063f69
marinaPerezYnov <148948211+marinaPerezYnov@users.noreply.github.com>
2024-11-22 11:54:06 +0100

    advanced security token

--- BackEnd/apiFolder/config/bundles.php
+++ BackEnd/apiFolder/config/bundles.php
@@ -15,4 +15,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
